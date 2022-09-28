3c41706830e1bbd34af0c0685df576005c3fd535
Shawn Chi <shawnchi@shawns-mbp-2.lan>
2022-09-28 00:18:42 -0700

    neon grame

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -21,6 +21,7 @@ function LandingPage() {
 
     return (
         <>  
+            <NeonFrame/>
             <Wrapper>
                 {/* <TypeWriter text={'Hi, This is Shawn. I specialize in the design and development of digital interfaces. Please feel free to poke around at my website!'}/> */}
                 <DynamicHeader big={true} current='home'></DynamicHeader>
@@ -41,6 +42,53 @@ const Wrapper = styled.div`
     display: grid;
     place-content: center;
 
+`
+const NeonFrame = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    width: 90%;
+    height: 90%;
+    border: 4px var(--grey100) solid;
+    border-radius: 8px;
+    filter: drop-shadow(0px 0px 16px var(--pink));
+    animation: flicker 3s infinite alternate;
+
+
+    @keyframes flicker {
+        0% {
+            opacity: 1;
+            border: 4px var(--grey100) solid;
+            /* filter: drop-shadow(0px 0px 32px var(--pink)); */
+
+        }
+
+        68% {
+            opacity: 1;
+            /* filter: drop-shadow(0px 0px 16px var(--pink)); */
+
+
+        }
+        70% {
+            opacity: 0.8;
+            /* border: 4px var(--grey100) solid; */
+            /* filter: drop-shadow(0px 0px 16px var(--pink)); */
+
+
+        }
+        75% {
+            opacity: 1;
+            /* border: 4px var(--grey100) solid; */
+            /* filter: drop-shadow(0px 0px 16px var(--pink)); */
+        }
+        75% {
+            opacity: 1;
+            border: 4px pink solid;
+            /* filter: drop-shadow(0px 0px 16px var(--pink)); */
+        }
+
+    }
 `
 
 export default LandingPage
